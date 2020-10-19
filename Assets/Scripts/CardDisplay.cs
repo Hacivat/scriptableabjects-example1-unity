96483936b25c589ff58907b28bacebd94af6cc38
Osman Pekaydın <pekaydin580@gmail.com>
2020-10-19 13:08:42 +0300

    Initial commit

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+    void Start()
+    {
+        card.Print();
+    }
+}
